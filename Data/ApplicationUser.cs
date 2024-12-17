eb72ee160af962baf6e03f26b839d22969aeced4
peter pegues <peter.pegues@gmail.com>
2024-12-17 09:30:20 -0500

    Add project files.

---
+++ Data/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace BabyData.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+}
---
+++ Data/FeedingRecord.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BabyData.Data
+{
+    public class FeedingRecord
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public Guid UserId { get; set; }
+        public TimeOnly StartTime { get; set; }
+        public TimeOnly EndTime { get; set; }
+        public DateOnly Date {  get; set; }
+        public FeedingType FeedingType { get; set; }
+        public string Notes { get; set; } = string.Empty;
+    }
+
+    public enum FeedingType
+    {
+        None = 0,
+        Breast = 1,
+        Bottle = 2,
+        Solid = 3,
+    }
+}
