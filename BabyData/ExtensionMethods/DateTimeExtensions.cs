78a0816868a323c7fdbac23a4c2dad1212785d5c
peter pegues <peter.pegues@gmail.com>
2025-01-19 16:03:28 -0500

    useded human readable for all timespans made short and long versions

--- BabyData/ExtensionMethods/DateTimeExtensions.cs
+++ BabyData/ExtensionMethods/DateTimeExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace BabyData.ExtentionMethods
+﻿using ApexCharts;
+
+namespace BabyData.ExtentionMethods
 {
     public static class DateTimeExtensions
     {
@@ -14,18 +16,36 @@
             var totalMinuets = ts.TotalMinutes;
             int hours = (int)(totalMinuets / 60);
             int mins = (int)(totalMinuets % 60);
-
             
             if( hours == 0 )
-            {
-                return $"{mins} mins";
-            }
+                return $"{mins}m";
+    
             if (hours != 0 && mins == 0)
-            {
-                return $"{hours} hours";
-            }
+                return $"{hours}h";
 
-            return  $"{hours} hours and {mins} min";
+            return  $"{hours}h {mins}m";
+
+        }
+
+        public static string ToHumanReadableLong(this TimeSpan ts)
+        {
+            var totalMinuets = ts.TotalMinutes;
+            int hours = (int)(totalMinuets / 60);
+            int mins = (int)(totalMinuets % 60);
+
+            var minOut = $"{mins} minute";
+            minOut = mins > 1 ? minOut + "s" : minOut;
+
+            var hrsOut = $"{hours} hour";
+            hrsOut =  hours > 1 ? hrsOut + "s" : hrsOut;
+
+            if (hours == 0)
+                return minOut;
+
+            if (hours != 0 && mins == 0)
+                return hrsOut;
+
+            return hrsOut + " " + minOut;
 
         }
     }
