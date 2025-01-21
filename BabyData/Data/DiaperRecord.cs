b90b8b526832376a60b75df3b84dc4f162f8edc8
peter pegues <peter.pegues@gmail.com>
2025-01-21 09:15:46 -0500

    added diaper records pages

--- BabyData/Data/DiaperRecord.cs
+++ BabyData/Data/DiaperRecord.cs
@@ -4,14 +4,18 @@ namespace BabyData.Data
 {
     public class DiaperRecord : UtcAndLocalDateTimeRecord, IHaveBabyData
     {
+        public DiaperRecord()
+        {
+            Tags = new List<DiaperTag>();
+        }
         [Key]
         public Guid Id { get; set; }
         [Required]
         public Guid UserId { get; set; }
         [Required]
         public Guid BabyId { get; set; }
-        public string ColorCode { get; set; }
-        public string Notes { get; set; }
+        public string ColorCode { get; set; } = "#F1F7E9";
+        public string Notes { get; set; } = "";
         public decimal Weight { get; set; }
         public ICollection<DiaperTag> Tags { get; }
     }
