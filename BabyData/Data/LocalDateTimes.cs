4782192110b353854e4952745c8771f9c5df1bae
peter pegues <peter.pegues@gmail.com>
2025-01-29 21:20:46 -0500

    time adjuster changes

--- BabyData/Data/LocalDateTimes.cs
+++ BabyData/Data/LocalDateTimes.cs
@@ -9,15 +9,5 @@
         public string EndTimeString => EndTimeLocal.ToString("hh:mm tt");
         public DateTime StartTimeLocal { get; set; }
         public string StartTimeString => StartTimeLocal.ToString("hh:mm tt");
-
-        public void NudgeEndTime(int min)
-        {
-            EndTimeLocal += TimeSpan.FromMinutes(min);
-        }
-
-        public void NudgeStartTime(int min)
-        {
-            StartTimeLocal += TimeSpan.FromMinutes(min);
-        }
     }
 }
